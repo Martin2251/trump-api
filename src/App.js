0b4e5c7c9978f54a2eec83d75f7d6daae2de47ae
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2021-12-28 14:00:21 +0000

    api done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState} from "react"
+import axios from "axios"
 
 function App() {
+  const [quote , setQuote] = useState("")
+
+  const getQuote = () => {
+    axios.get("https://api.whatdoestrumpthink.com/api/v1/quotes/random")
+    .then(response => {console.log(response.data.message)
+    setQuote(response.data.message)
+  }).catch(err => {
+console.log(err)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getQuote}>get quote</button>
+      {quote && <h4>the idiot says "{quote}"</h4>}
     </div>
   );
 }
